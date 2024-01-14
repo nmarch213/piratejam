4bf190279246360336be939f946143f909bdb00e
Nicholas March <rival@passion.lan>
2024-01-13 21:54:59 -0800

    avoid placed obs but path not updating properly

--- enemies/ember/ember.gd
+++ enemies/ember/ember.gd
@@ -1,13 +1,20 @@
 extends Enemy 
 
-const speed = 20
+@export var speed = 100
 
 @export var tree: MotherTree
 @onready var nav_agent: EnemyNavigationComponent = $EnemyNavigationComponent
 
+func _ready() -> void:
+	nav_agent.connect("velocity_computed", move)
+
 func _physics_process(_delta: float) -> void:
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
+	nav_agent.set_velocity(velocity)
+
+func move(newVelocity: Vector2) -> void:
+	velocity = newVelocity
 	move_and_slide()
 
 func make_path() -> void:
