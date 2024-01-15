d44ae6a69008db1050314c4bf4353fd5adc817e0
wharfjetty <wharfjetty@gmail.com>
2024-01-15 17:49:41 -0500

    cleanup of some logic

--- SunlightLabel.gd
+++ SunlightLabel.gd
@@ -1,10 +1,11 @@
 extends Label
 
 var max_length = 1
+signal sunlight_text_max_changed(length)
 
 func _process(delta):
 	text = str(SunlightManager.sunlight_banked)
 	if text.length() > max_length:
 		max_length = text.length()
-		get_parent().get_parent().get_node("Bubble").transform.x.x = 1 + (.2 * max_length)
+		sunlight_text_max_changed.emit(max_length)
 		position.x = 52 + max_length / 2
