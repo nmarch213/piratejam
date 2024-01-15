726308cf45b14ba317e8fdedcf4d8cc4eb3fdd28
troy <53122105+troy-herbison@users.noreply.github.com>
2024-01-15 13:42:40 -0600

    buySapling (#12)
    * Working place sapling at mouse position
    
    * Working buy menu and placement after buying
    
    * Working buy menu and placement after buying

---
+++ BuildingController.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+@onready var tile_map = $"../TileMap"
+@onready var main = $".."
+@onready var ui_controller = $"../UIController"
+
+
+
+func _on_buy_menu_id_pressed(id):
+	# if the user clicked sapling in the menu
+	if(id == 0):
+		place_sapling_tower(ui_controller.latest_cell_clicked)
+	
+	
+func place_sapling_tower(cell_clicked_position: Vector2i) -> void:
+	tile_map.set_cell(tile_map.LAYER.TOWERS, cell_clicked_position, 3, Vector2(0,0), 1)
+	main.astar_grid.set_point_solid(cell_clicked_position, true)
+	spread_grass(cell_clicked_position)
+
+func spread_grass(cell_clicked_position: Vector2i) -> void:
+	for x in range(-1, 2):
+		for y in range(-1, 2):
+			await get_tree().create_timer(.1).timeout
+			var grass_pos = Vector2i(x, y) + cell_clicked_position
+			tile_map.set_cell(tile_map.LAYER.BASE, grass_pos, 4, Vector2(0,0), 0)	
--- TileMap.gd
+++ TileMap.gd
@@ -7,8 +7,15 @@ enum LAYER {
 	GRID = 3,
 }
 
+signal tile_map_clicked(cell_clicked_position: Vector2i, event_position: Vector2)
+
 func _process(_delta):
-	var tile := local_to_map(get_local_mouse_position())
+	var cell : Vector2i = local_to_map(get_local_mouse_position())
 	clear_layer(LAYER.GRID)
-	if tile:
-		set_cell(LAYER.GRID, tile, 4, Vector2i(1,0), 0)
+	if cell:
+		set_cell(LAYER.GRID, cell, 4, Vector2i(1,0), 0)
+		
+		
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		tile_map_clicked.emit(local_to_map(get_local_mouse_position()), event.position)
