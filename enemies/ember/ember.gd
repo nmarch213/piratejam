9fbc0ecb0dfb0f547ab33a101a857f066db693e2
wharfjetty <109560320+wharfjetty@users.noreply.github.com>
2024-01-16 23:20:14 -0500

    Add flame enemies, make spawner spawn random combinations of enemy types (#28)
    * make enemies move at a defined speed
    
    simplify speed code in the process
    
    * Add flame enemies, make spawner spawn random combinations of enemy types

--- enemies/ember/ember.gd
+++ enemies/ember/ember.gd
@@ -1,6 +1,6 @@
 extends Enemy 
 
-var blaze: PackedScene = preload("res://enemies/blaze/Blaze.tscn")
+var flame: PackedScene = preload("res://enemies/flame/flame.tscn")
 
 var rng = RandomNumberGenerator.new()
 
@@ -9,7 +9,7 @@ func _ready():
 	
 func _on_respawn_timer_timeout():
 	if rng.randf() > 0.66:
-		var respawned_foe = blaze.instantiate()
+		var respawned_foe = flame.instantiate()
 		respawned_foe.position = global_position
 		get_parent().add_child(respawned_foe)
 		queue_free()
---
+++ enemies/flame/flame.gd
@@ -0,0 +1,12 @@
+extends Enemy
+
+var ember: PackedScene = preload("res://enemies/ember/ember.tscn")
+
+func _ready():
+	speed = 0.5
+	$HealthComponent.death.connect(_on_health_component_death)
+
+func _on_health_component_death():
+	var remains = ember.instantiate()
+	remains.position = global_position
+	get_parent().add_child(remains)
