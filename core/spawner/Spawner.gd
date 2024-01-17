85f9dab19622389e3dc78d33bfe18f31a650d2f2
wharfjetty <wharfjetty@gmail.com>
2024-01-16 20:33:25 -0500

    Add flame enemies, make spawner spawn random combinations of enemy types

--- core/spawner/Spawner.gd
+++ core/spawner/Spawner.gd
@@ -1,6 +1,8 @@
 extends Marker2D
 
 var blaze: PackedScene = preload("res://enemies/blaze/Blaze.tscn")
+var flame: PackedScene = preload("res://enemies/flame/flame.tscn")
+
 @onready var wave_timer: Timer = $WaveTimer;
 
 var current_wave = 0
@@ -11,13 +13,26 @@ func _ready():
 
 
 func spawn_wave_1():
-	var total_enemies = 5
-
-	for e in total_enemies:
-		var enemy = blaze.instantiate()
-		enemy.position = global_position
+	var wave_strength = 10
+	# blaze str = 2
+	# flame = 1
+	
+	var blazes = randi_range(0, wave_strength) / 2
+	var flames = wave_strength - (blazes * 2)
+	for a_blaze in blazes:
+		spawn_enemy(blaze)
 		await get_tree().create_timer(.3).timeout
-		get_parent().add_child(enemy)
+
+	for a_flame in flames:
+		spawn_enemy(flame)
+		await get_tree().create_timer(.3).timeout
+		
+func spawn_enemy(enemy_type: PackedScene):
+	var enemy = enemy_type.instantiate()
+	enemy.position = global_position
+	enemy.position.x = randfn(enemy.position.x, 10)
+	enemy.position.y = randfn(enemy.position.y, 10)
+	get_parent().add_child(enemy)
 
 func _on_wave_timer_timeout():
 	current_wave += 1
