41ca8f10be0c07855fd59fbc139f9b8cc44f222b
Troy Herbison <troy.herbison@gmail.com>
2024-01-20 16:40:34 -0600

    Add win condition at 10 minutes

--- game_manager.gd
+++ game_manager.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @onready var mother_tree: MotherTree = $"../MotherTree"
 @onready var game_over_screen = $GameOverScreen
+@onready var game_over_screen_label = $GameOverScreen/Label
 @onready var player_camera = $"../PlayerCamera"
 @onready var tile_click_controller = $"../TileClickController"
 
 var game_time: float = 0.0
+const GAME_WIN_CODITION_SECONDS: float = 600.0
 
 
 func _ready():
@@ -15,11 +17,26 @@ func _ready():
 	
 func _process(delta):
 	game_time += delta
+	
+	if game_time >= GAME_WIN_CODITION_SECONDS:
+		show_game_won_screen()
 
 	
 func _on_mother_tree_exited():
+	end_game("Game Over\nThe mother tree died :(")
+	
+	
+func show_game_won_screen():
+	end_game("You won!\nNice job on spreading for 10,000 years!")
+	
+	
+func end_game(game_over_text: String):
 	player_camera.can_move_camera = false
 	player_camera.zoom = Vector2(1,1)
 	game_over_screen.position = player_camera.position
-	tile_click_controller.queue_free()
+	game_over_screen_label.text = game_over_text
+	if is_instance_valid(tile_click_controller):
+		tile_click_controller.queue_free()
 	game_over_screen.show()
+	
+	
