8110b64a9c31e960115269d887bbcfe8b0c486f7
Troy Herbison <troy.herbison@gmail.com>
2024-01-20 10:11:51 -0600

    Specify thorn damage location and updated thorn damage

--- tree/towers/thorn/thorn.gd
+++ tree/towers/thorn/thorn.gd
@@ -6,4 +6,4 @@ class_name Thorn
 @onready var bullet = load("res://tree/towers/thorn/thorn_bullet.tscn")
 
 func _load_bullet():
-	attack_component.bullet = bullet;
+	attack_component.bullet = bullet
--- tree/towers/thorn/thorn_bullet.gd
+++ tree/towers/thorn/thorn_bullet.gd
@@ -3,3 +3,6 @@ class_name ThornBullet
 
 func _ready():
 	self.body_entered.connect(_on_bullet_body_entered)
+	# Modify this value to change the power of thorn attacks. This value is later used by attack 
+	# component. Direct updates to attack component are not functioning
+	self.bullet_damage = 10
