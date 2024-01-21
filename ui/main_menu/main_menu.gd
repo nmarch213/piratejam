3dae6a4877f838a0d2cefadb78e829c91c3c0a5d
wharfjetty <109560320+wharfjetty@users.noreply.github.com>
2024-01-21 17:32:48 -0500

    Add a main menu (#60)
    - Based on my menu in residual werewolves
    - Animated
    - No sound yet
    - Not implemented as starting scene
    
    - Made change to project so UI items scale with window size

---
+++ ui/main_menu/main_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+func _ready():
+	$MainMenuAnimation.play("menu_display_animation")
+
+func _process(_delta):
+	if Input.is_action_pressed("esc"): # skip to the end of the animation
+		$MainMenuAnimation.advance(4.5)
+
+func _on_how_to_play_button_pressed():
+	pass # Replace with function body.
+	#TODO: Make the "How To Play" section visible (also todo: make these nodes)
+
+func _on_quit_button_pressed():
+	$MainMenuAnimation.play("quit_animation")
+	
+
+func _on_begin_button_pressed():
+	$MainMenuAnimation.play("begin_animation")
+
+func _on_main_menu_animation_animation_finished(anim_name):
+	match anim_name:
+		"quit_animation":
+			get_tree().quit()
+		"begin_animation":
+			get_tree().change_scene_to_file("res://main.tscn")
+		#"menu_display_animation"
+			#pass
